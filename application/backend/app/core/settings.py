51a18b7bfe6c1d7388f768593ee81417f8ed2730
k1llzzy <gekachan04@gmail.com>
2024-03-13 19:59:53 +0300

    [1-5] :: Ручка регистрации

--- application/backend/app/core/settings.py
+++ application/backend/app/core/settings.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 
-engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/gremcasino')
+engine = create_engine(
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/gremcasino"
+)
 
 Base = declarative_base()
